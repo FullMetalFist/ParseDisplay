6b6a79264447df9f414963b56f244e7f5532dac3
Michael Vilabrera <m.vilabrera@gmail.com>
2015-05-11 13:51:09 -0400

    initial commit, functional programmatic view controller

---
+++ ParseDisplay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ParseDisplay
+//
+//  Created by Michael Vilabrera on 5/11/15.
+//  Copyright (c) 2015 Giving Tree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ParseDisplay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParseDisplay
+//
+//  Created by Michael Vilabrera on 5/11/15.
+//  Copyright (c) 2015 Giving Tree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
